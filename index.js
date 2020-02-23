3853f6e44fae4a975a8cfecc36ff07b85bb9117a
Getaneh <54732665+getchales@users.noreply.github.com>
2020-02-23 09:21:00 -0600

    Add files via upload
    express

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-const Loger = require('./loger');
-const loger = new Loger();
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const logger = require('./middleware/logger');
+const members = require('./Members');
+const app = express();
 
-loger.on('message', data => console.log('Called Listener', data));
-loger.log("Hello World");
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+//Body parser middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+//Homepage route
+app.get('/', (req, res) =>
+    res.render('index', {
+        title: 'Member App',
+        members
+    }));
+
+//set static folders
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/members', require('./routes/api/members'));
+const PORT = process.env.PORT || 5002;
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
