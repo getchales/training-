b4a84e1002f5aa4015a65e6e24dce59c148319ad
Getaneh <54732665+getchales@users.noreply.github.com>
2020-02-23 09:21:39 -0600

    Add files via upload
     express training

---
+++ Members.js
@@ -0,0 +1,21 @@
+const members = [
+    {
+        id: 1,
+        name: 'Getaneh Tafere',
+        email: 'getchales@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Bikale ',
+        email: 'bikale@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 3,
+        name: 'Frie',
+        email: 'frehiwot@gmail.com',
+        status: 'active'
+    }
+];
+module.exports = members; 
